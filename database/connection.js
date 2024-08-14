5b3b7ce2f7db8d9cf883714f19d373e26067185e
Nicovel <nicovel98@gmail.com>
2024-08-14 07:29:40 -0500

    conecction to database

---
+++ database/connection.js
@@ -0,0 +1,17 @@
+import { connect } from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const connection = async () => {
+    try {
+        await connect(process.env.MONGODB_URI);
+        console.log("MongoDB connected to red_social");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error.message);
+        /* res.status(500).send("Server Error"); */
+        throw new Error("Error connecting to MongoDB!");
+    }
+}
+
+export default connection;
---
+++ start-project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Cambiar al directorio del proyecto Node.js y ejecutar el script
+echo "Iniciando la aplicación Node.js..."
+cd /home/nico/Escritorio/EAN/TalentoTech/Entregables/Ejercicios/red_social_back
+npm run start:watch
