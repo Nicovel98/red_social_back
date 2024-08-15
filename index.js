c0bdb3b9556eeca3fdc7021b32f3769dbac22ff2
Nicovel <nicovel98@gmail.com>
2024-08-15 07:14:44 -0500

    Thunder client approved the request

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connection from "./database/connection.js";
-
+import bodyParser from "body-parser";
+import cors from "cors";
 // Mensaje de bienvenida para verificar que ejecuta bien la API de Node
 console.log("API Node is running...");
 
@@ -11,7 +12,24 @@ connection();
 const app = express();
 const port = process.env.PORT || 3900;
 
+// Configurar Cors
+app.use(cors());
 
+//Decodificar los datos desde los formularios para convertirlos en JS
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Rutas de la API
+
+app.get('/ruta-prueba', (req, res) => {
+    return res.status(200).json(
+        {
+            'id': 1,
+            'name': 'Nico',
+            'username': 'Nicovel'
+        }
+    );
+});
 
 // Configuración del servidor de Node
 app.listen(port, () => {
