4e1bf7dc34bdd58ec7fae40690fc00aa8d673fa6
Nicovel <nicovel98@gmail.com>
2024-08-20 07:14:18 -0500

    Login usuario

--- controllers/user.js
+++ controllers/user.js
@@ -17,7 +17,8 @@ export const registerUser = async (req, res) => {
 
         // Crear el objeto de usuario con los datos que ya validamos
         let user_to_save = new User(params);
-
+        // Convierte el email del objeto en Minúsculas
+        user_to_save.email = params.email.toLowerCase();
         // Busca si ya existe un usuario con el mismo email o nick
         const existingUser = await User.findOne({
             $or: [
@@ -63,3 +64,54 @@ export const registerUser = async (req, res) => {
 }
 
 // Método Login de Usuarios
+export const loginUser = async (req, res) => {
+    try {
+        // Obtener los datos de la petición
+        let params = req.body;
+
+        // Validaciones de los datos obtenidos
+        if (!params.email || !params.password) {
+            return res.status(400).send({
+                status: "error",
+                message: "Faltan datos por enviar"
+            });
+        }
+
+        // Busca un usuario con el email que se pasa en la petición
+        const user = await User.findOne({ email: params.email.toLowerCase() });
+
+        // Si no encuentra un usuario, devuelve un mensaje indicando que el usuario no existe
+        if (!user) {
+            return res.status(404).send({
+                status: "error",
+                message: "!El usuario no existe!"
+            });
+        }
+
+        // Compara la contraseña cifrada del usuario
+        const matchPass = await bcrypt.compare(params.password, user.password);
+
+        // Si no coincide la contraseña, devuelve un mensaje indicando que la contraseña es incorrecta
+        if (!matchPass) {
+            return res.status(401).send({
+                status: "error",
+                message: "!La contraseña es incorrecta!"
+            });
+        }
+
+        // Devolver el usuario logueado
+        return res.status(200).json({
+            status: "success",
+            message: "Login exitoso",
+            user
+        });
+    } catch (error) {
+        // Manejo de errores
+        console.log("Error en el login de usuario:", error);
+        // Devuelve mensaje de error
+        return res.status(500).send({
+            status: "error",
+            message: "Error en el login de usuario"
+        });
+    }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 const router = Router();
-import { registerUser } from "../controllers/user.js";
+import { registerUser, loginUser } from "../controllers/user.js";
 
 // Definir las rutas
 /*router.get('/test-user', testUser);*/
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 // Exportar el Router
 export default router;
